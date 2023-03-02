afe6d54b1f2a0d454a561ce19145b72574dfe67a
Chris Sousa <chris.sousa@omegaenergia.com.br>
2023-03-02 09:15:46 -0300

    Minor adjustments

---
+++ README.md
@@ -0,0 +1,10 @@
+# MFE Experiment Container
+
+This repo is a container for the micro front ends collected below.
+You can check them running *in the container* through here:
+- [MFE Container in GitHub Pages](https://codechriscode.github.io/mfe-chat)
+
+## MFE Collection
+
+- [Chat MFE](https://github.com/codechriscode/mfe-chat)
+- [Movie List MFE](https://github.com/codechriscode/mfe-movie-list)
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+const mfeAddresses = {
+  movies: "https://codechriscode.github.io/mfe-movie-list/",
+  chat: "https://codechriscode.github.io/mfe-chat/",
+};
+
+type MfesType = keyof typeof mfeAddresses;
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentMfe, setCurrentMfe] = useState<MfesType | null>(null);
 
   return (
     <div className="App">
       <div className="navBar">
         <p className="logo">✨</p>
         <div className="appList">
-          <button className="appButton" onClick={() => {/*renderMfe("movies")*/}}>
+          <button
+            className="appButton"
+            onClick={() => {
+              /*renderMfe("movies")*/
+            }}
+          >
             🍿
           </button>
-          <button className="appButton" onClick={() => {/*renderMfe("about")*/}}>
+          <button
+            className="appButton"
+            onClick={() => {
+              /*renderMfe("about")*/
+            }}
+          >
             📝
           </button>
         </div>
       </div>
-      <div className="hosted-app"></div>
+      <div className="hosted-app">
+        <h1 style={{ marginLeft: "1em" }}>
+          Bem-vindo/a!<br/>Navegue a vontade entre widgets e paineis.
+        </h1>
+      </div>
     </div>
   );
 }
@@ -26,4 +48,4 @@ function App() {
 type ContainerWindowType = typeof window & { isInContainer: boolean };
 (window as ContainerWindowType).isInContainer = true;
 
-export default App
+export default App;
